64b59b614b5cee07fc6b07510bea24272b1c60fd
MzahirA <zahirabdullahsysy@gmail.com>
2024-05-18 18:58:27 +0700

    firsit init github

---
+++ js/main.js
@@ -0,0 +1,197 @@
+(function ($) {
+ "use strict";
+    
+/*-----------------------------
+	Menu Stick
+---------------------------------*/
+    if ($(".sticker")[0]){
+        $('.sticker');
+        $(window).scroll(function(){
+            var wind_scr = $(window).scrollTop();
+            var window_width = $(window).width();
+            var head_w = $('.sticker').height();
+            if (window_width >= 10) {
+                if(wind_scr < 400){
+                    if($('.sticker').data('stick') === true){
+                        $('.sticker').data('stick', false);
+                        $('.sticker').stop(true).animate({opacity : 0}, 300, function(){
+                            $('.sticker').removeClass('stick slideDown');
+                            $('.sticker').stop(true).animate({opacity : 1}, 300);
+                        });
+                    }
+                } else {
+                    if($('.sticker').data('stick') === false || typeof $('.sticker').data('stick') === 'undefined'){
+                        $('.sticker').data('stick', true);
+                        $('.sticker').stop(true).animate({opacity : 0},300,function(){
+                            $('.sticker').addClass('stick slideDown');
+                            $('.sticker.stick').stop(true).animate({opacity : 1}, 300);
+                        });
+                    }
+                }
+            }
+        });
+    };	
+    
+/*----------------------------
+    jQuery MeanMenu
+------------------------------ */
+    $('.mobile-menu nav').meanmenu({
+        meanScreenWidth: "990",
+        meanMenuContainer: ".mobile-menu",
+        onePage: true,
+    });
+	
+/*----------------------------
+    wow js active
+------------------------------ */
+    new WOW().init();
+ 
+/*----------------------------
+    Owl Feature List active
+------------------------------ */  
+    $(".feature-list-carousel").owlCarousel({
+        autoPlay: false, 
+        slideSpeed:2000,
+        pagination:true,
+        navigation:false,	  
+        items : 1,
+        /* transitionStyle : "fade", */    /* [This code for animation ] */
+        navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+        itemsDesktop : [1199,1],
+        itemsDesktopSmall : [980,1],
+        itemsTablet: [768,1],
+        itemsMobile : [479,1],
+    });
+    
+/*----------------------------
+    Blog active
+------------------------------ */  
+    $(".blog-carousel").owlCarousel({
+        autoPlay: false, 
+        slideSpeed:2000,
+        pagination:false,
+        navigation:false,	  
+        items : 3,
+        /* transitionStyle : "fade", */    /* [This code for animation ] */
+        navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+        itemsDesktop : [1199,3],
+        itemsDesktopSmall : [991,2],
+        itemsTablet: [768,2],
+        itemsMobile : [600,1],
+    });
+    
+/*--------------------------
+    scrollUp
+---------------------------- */	
+	$.scrollUp({
+        scrollText: '',
+        easingType: 'linear',
+        scrollSpeed: 900,
+        animation: 'fade'
+    }); 
+    
+    
+/*--------------------------------
+	Testimonial Slick Carousel
+-----------------------------------*/
+    $('.testimonial-text-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        draggable: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+/*------------------------------------
+	Testimonial Slick Carousel as Nav
+--------------------------------------*/
+    $('.testimonial-image-slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.testimonial-text-slider',
+        dots: false,
+        arrows: true,
+        centerMode: true,
+        focusOnSelect: true,
+        centerPadding: '10px',
+        responsive: [
+            {
+              breakpoint: 450,
+              settings: {
+                dots: false,
+                slidesToShow: 3,  
+                centerPadding: '0px',
+                }
+            },
+            {
+              breakpoint: 420,
+              settings: {
+                autoplay: true,
+                dots: false,
+                slidesToShow: 1,
+                centerMode: false,
+                }
+            }
+        ]
+    });
+    
+/*--------------------------------
+	One Page Nav
+-----------------------------------*/
+    var top_offset = $('.main-menu').height() - -60;
+    $('.main-menu nav ul').onePageNav({
+        currentClass: 'active',
+        scrollOffset: top_offset,
+    });
+
+
+	/**********************
+	* Contact Form
+	***********************/
+
+	$(function () {
+        // Get the form.
+        var form = $('#contact-form');
+        // Get the messages div.
+        var formMessages = $('.form-messege');
+        // Set up an event listener for the contact form.
+        $(form).submit(function (e) {
+            // Stop the browser from submitting the form.
+            e.preventDefault();
+            // Serialize the form data.
+            var formData = $(form).serialize();
+            // Submit the form using AJAX.
+            $.ajax({
+                type: 'POST',
+                url: $(form).attr('action'),
+                data: formData,
+            })
+                .done(function (response) {
+                    // Make sure that the formMessages div has the 'success' class.
+                    $(formMessages).removeClass('error');
+                    $(formMessages).addClass('success');
+    
+                    // Set the message text.
+                    $(formMessages).text(response);
+    
+                    // Clear the form.
+                    $('#contact-form input,#contact-form textarea').val('');
+                })
+                .fail(function (data) {
+                    // Make sure that the formMessages div has the 'error' class.
+                    $(formMessages).removeClass('success');
+                    $(formMessages).addClass('error');
+    
+                    // Set the message text.
+                    if (data.responseText !== '') {
+                        $(formMessages).text(data.responseText);
+                    } else {
+                        $(formMessages).text(
+                            'Oops! An error occured and your message could not be sent.'
+                        );
+                    }
+                });
+        });
+    });
+ 
+})(jQuery); 
